d905f0f930d251cc9e0dacf0328e0b1e17122a66
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-13 19:32:04 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,8 +5,7 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
-    @resp = Faraday.get
-    'https://api.github.com/search/repositories' do |req|
+    @resp = Faraday.get('https://api.github.com/search/repositories') do |req|
       req.params['client_id'] = "Iv1.96ed4cd4419b4051"
       req.params['client_secret'] = "382dd66c67b6c030786f4807ef455fcefa307258"
       req.params['q'] = params[:query]
