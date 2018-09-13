ac4daeb28d5463f57df7db5ce3798d723b4a897f
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-13 19:32:32 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -12,6 +12,6 @@ class RepositoriesController < ApplicationController
       end
     body_hash = JSON.parse(@resp.body)
     @repositories = body_hash["items"]
-    render 'search'
+    render :search
   end
 end
